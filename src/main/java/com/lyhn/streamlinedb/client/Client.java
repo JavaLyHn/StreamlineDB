a46cec65551f5f1afbd7a352fc7fce9aeec608be
JavaLyHn <2443560753@qq.com>
2026-01-15 18:58:47 +0800

    完成客户端收发消息

--- src/main/java/com/lyhn/streamlinedb/client/Client.java
+++ src/main/java/com/lyhn/streamlinedb/client/Client.java
@@ -1,59 +1,28 @@
 package com.lyhn.streamlinedb.client;
 
-import com.lyhn.streamlinedb.transport.Encoder;
-import com.lyhn.streamlinedb.transport.Package;
 import com.lyhn.streamlinedb.transport.Packager;
-import com.lyhn.streamlinedb.transport.Transporter;
-
-import java.io.IOException;
-import java.net.Socket;
-import java.util.Scanner;
-
-// 客户端
+import com.lyhn.streamlinedb.transport.Package;
 public class Client {
-    private Packager packager;
+    private RoundTripper rt;
 
-    public Client(Packager packager){
-        this.packager = packager;
+    public Client(Packager packager) {
+        this.rt = new RoundTripper(packager);
     }
 
-    public void run(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Connected to server. Type 'exit' to quit.");
-        while(true){
-            System.out.println("> ");
-            String input = scanner.nextLine();
-            if("exit".equalsIgnoreCase(input)){
-                break;
-            }
-
-            try {
-                Package request = new Package(input.getBytes(), null);
-                packager.send(request);
-
-                Package response = packager.receive();
-                if(response.getError() != null){
-                    System.out.println(response.getError().getMessage());
-                }else{
-                    System.out.println(new  String(response.getData()));
-                }
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
-                break;
-            }
+    public byte[] execute(byte[] stat) throws Exception {
+        Package pkg = new Package(stat, null);
+        // 与服务器通信
+        Package resPkg = rt.roundTrip(pkg);
+        if(resPkg.getError() != null) {
+            throw resPkg.getError();
         }
-        try{
-            packager.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        return resPkg.getData();
     }
 
-    public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("127.0.0.1", 9999);
-        Encoder encoder = new Encoder();
-        Transporter transporter = new Transporter(socket);
-        Packager packager = new Packager(transporter, encoder);
-        new Client(packager).run();
+    public void close() {
+        try {
+            rt.close();
+        } catch (Exception e) {
+        }
     }
 }
---
+++ src/main/java/com/lyhn/streamlinedb/client/ClientTest.java
@@ -0,0 +1,58 @@
+package com.lyhn.streamlinedb.client;
+
+import com.lyhn.streamlinedb.transport.Encoder;
+import com.lyhn.streamlinedb.transport.Package;
+import com.lyhn.streamlinedb.transport.Packager;
+import com.lyhn.streamlinedb.transport.Transporter;
+
+import java.net.Socket;
+import java.util.Scanner;
+
+// 客户端
+public class ClientTest {
+    private Packager packager;
+
+    public ClientTest(Packager packager){
+        this.packager = packager;
+    }
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Connected to server. Type 'exit' to quit.");
+        while(true){
+            System.out.println("> ");
+            String input = scanner.nextLine();
+            if("exit".equalsIgnoreCase(input)){
+                break;
+            }
+
+            try {
+                Package request = new Package(input.getBytes(), null);
+                packager.send(request);
+
+                Package response = packager.receive();
+                if(response.getError() != null){
+                    System.out.println(response.getError().getMessage());
+                }else{
+                    System.out.println(new  String(response.getData()));
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                break;
+            }
+        }
+        try{
+            packager.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("127.0.0.1", 9999);
+        Encoder encoder = new Encoder();
+        Transporter transporter = new Transporter(socket);
+        Packager packager = new Packager(transporter, encoder);
+        new ClientTest(packager).run();
+    }
+}
